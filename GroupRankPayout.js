85043a3d9101ceb6136959aa2620fc4a0c2e260e
Sceleratis <sceleratis@gmail.com>
2024-05-14 21:26:47 -0400

    Update to current version used

--- GroupRankPayout.js
+++ GroupRankPayout.js
@@ -1,4 +1,3 @@
-
 /*
   Performs a fixed amount group payout to all users in a specified group rank. 
   
@@ -8,10 +7,7 @@
   For each user, you will be prompted to confirm the payout with a simple OK/Cancel prompt. If you want this to be fully automatated simply replace the if (confirm()) line with if (true) or remove that if statement entirely. Up to you.
   
   This will not check your group funds before performing payouts, so make sure you actually have enough to cover it or it's just going to drain everything then error once empty. 
-
   Also note that the amount per user will be floored as you cannot payout fractions of Robux, so the actual total amount paid out may be less than what was specified.
-
-
   How to use:
   Simply login to your Roblox account, open up the developer console in whatever browser you're using (usually can be done by pressing F12)
   and paste the contents of this script into the console after making any necessary config edits and press enter.
@@ -20,11 +16,19 @@
 */
 
 // Configuration options
-let funds_to_use = 100;   // The total amount of funds to divide among eligible users in the specified group role.
-let group = 1234567;      // The ID of your group.
-let role = 7654321;       // Note: this is the actual role ID (I yoinked mine from the request to get members in a rank) and is NOT the same as the rank number you set in the group settings.
+let funds_to_use = 100000;   // The total amount of funds to divide among eligible users in the specified group role.
+let group = 1234567;         // The ID of your group.
+let role = 8901234;          // Note: this is the actual role ID (I yoinked mine from the request to get members in a rank) and is NOT the same as the rank number you set in the group settings.
+let usersPerPage = 100;      // The number of users to grab per request when collecting rank users (Options are: 10, 20, 50, 100)
+let usersPerPayout = 15;     // Num users per payout.
+let delay = 30000;           // Delay between payouts in milliseconds.
+let ignoreUsers = [];        // Usernames of users to skip.
+let ignoredIds = [];         // Alternatively, User IDs of users to skip.
 
-let usersPerPage = 100; // The number of users to grab per request when collecting rank users (Options are: 10, 20, 50, 100)
+function ignoreUser(username, userId)
+{
+    return ignoreUsers.indexOf(username) > -1 || ignoredIds.indexOf(userId) > -1;
+}
 
 // Retrieves a list of users in the specified group rank.
 function getUsers(callback, curPage = "", users = []) {
@@ -51,16 +55,22 @@ function getUsers(callback, curPage = "", users = []) {
 function checkPayoutEligibility(users, callback, curUser = 0, eligible = []) {
     console.log("ELIGIBILITY CHECK", curUser, users.length);
     if (curUser >= users.length) {
+        console.log("DO CALLBACK");
         callback(eligible);
     } else {
         var user = users[curUser];
         $.ajax({
             url: `https://economy.roblox.com/v1/groups/${group}/users-payout-eligibility?userIds=${user.userId}`,
             success: function(data) {
+                console.log("SUCCESS", data);
                 if (data.usersGroupPayoutEligibility[user.userId] == "Eligible") {
+                    console.log("IS ELIGIBLE");
                     eligible.push(user);
-                    checkPayoutEligibility(users, callback, curUser + 1, eligible);
+                } else {
+                    console.log("NOT ELIGIBLE");
                 }
+
+                checkPayoutEligibility(users, callback, curUser + 1, eligible);
             },
             error: function(err) {
                 console.error("ELIGIBILITY ERROR", err);
@@ -91,6 +101,58 @@ function performPayout(user, amount) {
     });
 }
 
+function performMultiPayout(recipients, amount) {
+    var repStr = "";
+
+    for (i = 0; i < recipients.length; i++)
+    {
+       console.log(recipients[i].recipientId);
+       repStr += recipients[i].recipientId+","; 
+    }
+
+    if (confirm("Perform payout for "+repStr))
+    {
+        $.ajax({
+            url: `https://groups.roblox.com/v1/groups/${group}/payouts`,
+            method: "POST",
+            data: {
+                PayoutType: "FixedAmount",
+                Recipients: recipients
+            },
+            success: function(data) {
+                console.log("SUCCESSFULLY PERFORMED PAYOUT OF " + amount + " TO " + recipients);
+            },
+            error: function(err) {
+                console.error("PAYOUT ERROR", err);
+            }
+        });
+    }
+}
+
+function batch(array, chunkSize = 10) {
+    var chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        const chunk = array.slice(i, i + chunkSize);
+        chunks.push(chunk);
+    }
+    return chunks;
+}
+
+function doChunks(chunks, amount, chunkNum) {
+    if (chunkNum < chunks.length) {
+        console.log("PERFORMING PAYOUT! WAIT A MINUTE BEFORE CONTINUING (AVOID RATELIMIT)", chunks[chunkNum], amount);
+        performMultiPayout(chunks[chunkNum], amount);
+        var newNum = chunkNum + 1;
+        setTimeout(function() { doChunks(chunks, amount, newNum); }, delay);
+    }
+    else
+    {
+        console.log("MASS PAYOUT COMPLETE.");
+    }
+}
+
+var recipientList = [];
+
 // The actual process.
 getUsers(function(users){
     console.log("GOT USERS", users);
@@ -106,12 +168,21 @@ getUsers(function(users){
             var user = eligibleUsers[i];
           
             // Prompt us to send the payout to this user (disable/remove this if-else statement if you want to make this fully automatic)
-            if (confirm(`Payout ${amount} to ${user.username} (${user.userId})?`)) {
-                console.log("PAYOUT USER", user);
-                performPayout(user, amount);
+            if (ignoreUser(user.username, user.userId) == false && confirm(`Payout ${amount} to ${user.username} (${user.userId})?`)) {
+                console.log("PAYOUT USER", user, i);
+                //performPayout(user, amount);
+
+                recipientList.push({
+                    recipientId: user.userId,
+                    recipientType: "User",
+                    amount: amount
+                });
             } else {
                 console.log("SKIPPED USER", user);
             }
         }
+
+        var chunked = batch(recipientList, 20);
+        doChunks(chunked, amount, 0);
     });
 })
